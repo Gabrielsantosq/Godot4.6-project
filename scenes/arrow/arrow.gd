01f01580cf3694d92fdc1455063e65f979083250
Jõao Gabriel Nascimento dos santos <joaosantos@jacogames01.icat.art.br>
2026-02-06 11:16:24 -0300

    seila

--- scenes/arrow/arrow.gd
+++ scenes/arrow/arrow.gd
@@ -33,7 +33,7 @@ func _on_visible_on_screen_notifier_2d_screen_exited() -> void:
 func _on_body_entered(body: Node2D) -> void:
 	if body.is_in_group("enemy"):
 		body.take_damage_enemy(damage_crossbow)
-
+		queue_free()
 
 func _on_area_entered(area: Area2D) -> void:
 	pass
--- scenes/enemy/enemy.gd
+++ scenes/enemy/enemy.gd
@@ -9,6 +9,9 @@ var speed: float = 30
 var gravity: float = 980
 
 var is_dead: bool = false
+var inactive: bool = false
+var active: bool = false
+var walking: bool = false
 var is_attacking: bool = false
 var is_take_damage: bool = false
 
@@ -19,16 +22,17 @@ var health: int
 var state: States = States.INACTIVE
 
 
-enum States {INACTIVE,IDLE,WALK }
+enum States {INACTIVE,IDLE,WALK,TAKEDAMAGE, DEAD}
 
 func _ready() -> void:
 	health = max_health
 
 
 func _process(delta: float) -> void:
+	
 	gravity_force(delta)
 	move_to_player()
-	_state()
+	change_state()
 	move_and_slide()
 	change_anim()
 	flip()
@@ -38,9 +42,10 @@ func move_to_player():
 	if playerBody:
 		var direction = (playerBody.global_position - global_position).normalized()
 		velocity.x = direction.x * speed
+		
 	else:
 		velocity.x = 0
-
+		inactive = true 
 
 func gravity_force(delta):
 	if !is_on_floor():
@@ -49,16 +54,28 @@ func gravity_force(delta):
 		if velocity.y < 0:
 			velocity.y = 0
 
-func _state():
-	match state:
-		States.INACTIVE:
-			velocity.x = 0
-			
-		States.IDLE:
-			velocity.x =0
-			
-		States.WALK:
-			move_to_player()
+func change_state():
+	if is_dead:
+		state = States.DEAD
+		return
+		
+	if is_take_damage:
+		state =  States.TAKEDAMAGE
+		return
+		
+	if inactive:
+		state = States.INACTIVE
+		return
+		
+	if active:
+		state = States.IDLE
+		return
+		
+	if walking:
+		state = States.WALK
+		return
+
+
 func change_anim():
 	var anim = ""
 	
@@ -69,30 +86,42 @@ func change_anim():
 			anim = "idle"
 		States.WALK:
 			anim = "walk"
+		States.DEAD:
+			anim = "dead"
+		States.TAKEDAMAGE:
+			anim = "hurt"
 			
 	if animation_player.current_animation != anim:
 		animation_player.play(anim)
 
 func take_damage_enemy(damage: int):
+	is_take_damage = true
 	health -= damage
 	if health <= 0:
 		is_dead = true
-		await 5
-		queue_free()
+		is_take_damage = false
+		
 
+func dead():
+	$CollisionShape2D.disabled = true
+	await 2
+	queue_free()
 
 
 func _on_range_body_entered(body: Node2D):
 	if body.is_in_group("player"):
 		playerBody = body
-		state = States.IDLE
+		active = true
 		timer.start()
 
 
 func _on_range_body_exited(body: Node2D):
 	if body.is_in_group("player"):
 		playerBody = null
-		state = States.INACTIVE
+		inactive = true
+		active = false
+		walking = false
+
 
 func flip():
 	if velocity.x > 0:
@@ -103,4 +132,11 @@ func flip():
 
 func _on_timer_timeout() -> void:
 	if playerBody:
-		state = States.WALK
+		walking = true
+
+
+func _on_animation_player_animation_finished(anim_name: StringName) -> void:
+	if anim_name == "hurt":
+		is_take_damage = false
+	if anim_name == "dead":
+		dead()
